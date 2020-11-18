35a431b931d0276ac6c8955c633616efef65df38
Melvin <melvin.delpierre@signature-code.com>
2020-11-18 18:20:34 +0100

    oui

---
+++ app/src/main/java/com/example/intechcours/CameraActivity.kt
@@ -0,0 +1,71 @@
+package com.example.intechcours
+
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.DialogInterface
+import android.content.Intent
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+
+class CameraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+
+        val fragmentManager = supportFragmentManager;
+        val fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.add(R.id.bottom_bar, BottomBar());
+        fragmentTransaction.commit();
+
+        val button = findViewById<Button>(R.id.new_photo)
+        button.setOnClickListener {
+//            dispatchTakePictureIntent()
+            AlertDialog.Builder(this)
+                .setCancelable(false)
+                .setTitle("Confirmation")
+                .setMessage("Picture ?")
+                .setPositiveButton("Yes") { _: DialogInterface, i: Int ->
+                    val intent = Intent(applicationContext, SecondActivity::class.java)
+                    startActivity(intent)
+                }
+                .setNegativeButton("No", null)
+                .create()
+                .show()
+        }
+
+        val btn_newPhoto = findViewById<Button>(R.id.new_photo);
+        btn_newPhoto.setOnClickListener {
+            try {
+                val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(takePictureIntent, 1)
+            } catch (e: ActivityNotFoundException) {
+                // display error state to the user
+            }
+        }
+    }
+
+    val REQUEST_IMAGE_CAPTURE = 1
+
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            // display error state to the user
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap;
+            val imageView = findViewById<ImageView>(R.id.thumbTake)
+            imageView.setImageBitmap(imageBitmap)
+        }
+    }
+}
--- app/src/main/java/com/example/intechcours/MainActivity.kt
+++ app/src/main/java/com/example/intechcours/MainActivity.kt
@@ -13,82 +13,99 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
 
     private val disposable = CompositeDisposable();
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        val btn_click_me = findViewById<Button>(R.id.button)
-        val textView: TextView = findViewById(R.id.textView)
+        val fragmentManager = supportFragmentManager;
+        val fragmentTransaction = fragmentManager.beginTransaction();
 
-        textView.setText("ffff");
+        fragmentTransaction.add(R.id.bottom_bar, BottomBar());
+        fragmentTransaction.commit();
 
-        disposable.add(ApiService.searchMovie("pirate")
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .doOnError {
-            }
-            .subscribe ({
-                runOnUiThread {
-                    textView.text = it.results[0].title;
-                }
-            },Throwable::printStackTrace)
-        )
+//        FragmentTransaction fragmentTransaction = fragmentManager
 
-
-        btn_click_me.setOnClickListener {
-            AlertDialog.Builder(this)
-                .setCancelable(false)
-                .setTitle("Confirmation")
-                .setMessage("Picture ?")
-                .setPositiveButton("Yes") { _: DialogInterface, i: Int ->
-                    val intent = Intent(applicationContext, SecondActivity::class.java)
-                    startActivity(intent)
-                }
-                .setNegativeButton("No", null)
-                .create()
-                .show()
-
-        }
-
-        val bottonBar = findViewById<BottomNavigationView>(R.id.bottom_navigation);
-
-        bottonBar.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.page_1 -> {
-                    Log.i("0", "First");
-                    val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    try {
-                        startActivityForResult(takePictureIntent, 1)
-                    } catch (e: ActivityNotFoundException) {
-                        // display error state to the user
-                    }
-                    true
-                }
-                R.id.page_2 -> {
-                    Log.i("0", "Second")
-                    true
-                }
-                else -> false
-            }
-        }
+//        val btn_click_me = findViewById<Button>(R.id.button)
+//        val textView: TextView = findViewById(R.id.textView)
+//        val btn_Camera = findViewById<Button>(R.id.page_2)
+//
+//        btn_Camera.setOnClickListener {
+//            val intent = Intent(this, CameraActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+//        textView.setText("ffff");
+//
+//        disposable.add(ApiService.searchMovie("pirate")
+//            .subscribeOn(Schedulers.io())
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .doOnError {
+//            }
+//            .subscribe ({
+//                runOnUiThread {
+//                    textView.text = it.results[0].title;
+//                }
+//            },Throwable::printStackTrace)
+//        )
+//
+//
+//        btn_click_me.setOnClickListener {
+//            AlertDialog.Builder(this)
+//                .setCancelable(false)
+//                .setTitle("Confirmation")
+//                .setMessage("Picture ?")
+//                .setPositiveButton("Yes") { _: DialogInterface, i: Int ->
+//                    val intent = Intent(applicationContext, SecondActivity::class.java)
+//                    startActivity(intent)
+//                }
+//                .setNegativeButton("No", null)
+//                .create()
+//                .show()
+//
+//        }
+//
+//        val bottonBar = findViewById<BottomNavigationView>(R.id.bottom_navigation);
+//
+//        bottonBar.setOnNavigationItemSelectedListener { item ->
+//            when (item.itemId) {
+//                R.id.page_1 -> {
+//                    Log.i("0", "First");
+//                    val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                    try {
+//                        startActivityForResult(takePictureIntent, 1)
+//                    } catch (e: ActivityNotFoundException) {
+//                        // display error state to the user
+//                    }
+//                    true
+//                }
+//                R.id.page_2 -> {
+//                    Log.i("0", "Second")
+//                    true
+//                }
+//                else -> false
+//            }
+//        }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as Bitmap;
-            val imageView = findViewById<ImageView>(R.id.thumbTake)
-            imageView.setImageBitmap(imageBitmap)
-        }
-    }
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if (requestCode == 1 && resultCode == RESULT_OK) {
+//            val imageBitmap = data?.extras?.get("data") as Bitmap;
+//            val imageView = findViewById<ImageView>(R.id.thumbTake)
+//            imageView.setImageBitmap(imageBitmap)
+//        }
+//    }
 }
